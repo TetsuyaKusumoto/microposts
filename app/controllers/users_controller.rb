56d99f9da9a266bad4f1f1b5b4c369d420d390d4
tetsuyakusumoto <tetsuya.kusumoto@gmail.com>
2015-10-14 11:06:35 +0000

    add followings/followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  def edit
+  end
+  def update
+    if @user.update(user_params)
+      flash[:success] = "プロフィールの編集を完了しました"
+      redirect_to user_path(@user) 
+    else
+      render 'edit'
+    end
+  end
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts
@@ -20,6 +31,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location, :description)
+  end
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,18 @@ class User < ActiveRecord::Base
                                     foreign_key: "followed_id",
                                     dependent:   :destroy
   has_many :follower_users, through: :follower_relationships, source: :follower
+  # 他のユーザーをフォローする
+  def follow(other_user)
+    following_relationships.create(followed_id: other_user.id)
+  end
+
+  # フォローしているユーザーをアンフォローする
+  def unfollow(other_user)
+    following_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # あるユーザーをフォローしているかどうか？
+  def following?(other_user)
+    following_users.include?(other_user)
+  end
 end
